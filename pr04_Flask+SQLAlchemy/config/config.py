dcf6e38b3a0b50db354bdb1a330d1ede3d6d75d0
oriolvinmor <120462126+oriolvinmor@users.noreply.github.com>
2024-03-02 15:24:57 +0100

    Delete pr04_Flask+SQLAlchemy directory

--- pr04_Flask+SQLAlchemy/config/config.py
+++
@@ -1,8 +0,0 @@
-
-# Crear engine
-DATABASE = "oriolvm"
-USERNAME = "root"
-PASSWORD = ""
-DB_PORT = "3306"
-HOST = "localhost"
-DATABASE_URI = f"mysql://{USERNAME}:{PASSWORD}@{HOST}:{DB_PORT}/{DATABASE}"
